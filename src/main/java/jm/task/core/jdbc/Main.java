e27cb28de1e8ef4e705949b61e3f6571ea70e38d
Soslan171 <dzitsoev.98@bk.ru>
2023-07-18 16:46:01 +0300

    таск джбс

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", "Ivanov", (byte) 45));
+        users.add(new User("Tima", "Petrov", (byte) 44));
+        users.add(new User("Vadim", "Hasbik", (byte) 43));
+        users.add(new User("Pavlik", "Popov", (byte) 42));
+
+        for (User us : users) {
+            userService.saveUser(us.getName(), us.getLastName(), (byte) us.getAge());
+            System.out.println("User с именем – " + us.getName() + " добавлен в базу данных");
+        }
+
+        List<User> usersTable = userService.getAllUsers();
+        for (User us : usersTable) {
+            System.out.println(us);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String JDBC_DRIVER = "com.postgresql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
+            System.out.println("Connection OK!");
+        } catch (SQLException th) {
+            System.out.println("Connection ERROR!");
+            th.printStackTrace();
+        }
+
+        return connection;
+    }
 }
